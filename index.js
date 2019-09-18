e1ac3d2998612a335d8bdf05f83cc1fa427f46d3
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-09-18 08:46:48 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+const port = process.env.port || 3000
+
+const Tiwt = require('twit')
+const T = new Tiwt({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret,
+    timeout_ms: 60 * 1000
+})
+
+const stream = T.stream('statuses/filter', { track: '#apple' })
+stream.on('error', req => console.log(req))
+stream.on('tweet', tweet => {
+    console.log(tweet.user.name, tweet.text)
+    io.emit('tweet', {
+        username: tweet.user.name,
+        text: tweet.text
+    })
+})
+
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('home'))
+
+http.listen(port, (err) => {
+    if (err)
+        console.log(err)
+    else
+        console.log('Server Running')
+})
