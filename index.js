2fa050f919c2f99cc392be24b0d98092fafa2953
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-09-18 12:17:48 -0300

    Waiting permissions from Twitter

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const io = require('socket.io')(http)
 
 const port = process.env.port || 3000
 
+const streams = {}
+
+/*
 const Tiwt = require('twit')
 const T = new Tiwt({
     consumer_key: process.env.consumer_key,
@@ -14,7 +17,20 @@ const T = new Tiwt({
     access_token_secret: process.env.access_token_secret,
     timeout_ms: 60 * 1000
 })
+*/
 
+io.on('connection', socket => {
+    console.log(socket.id)
+    console.log(io.sockets.adapter.rooms)
+    socket.on('startStream', term => {
+        if (!(term in streams)) {
+            streams[term] = ''
+        }
+        console.log('startStream', term, streams)
+    })
+})
+
+/*
 const stream = T.stream('statuses/filter', { track: '#apple' })
 stream.on('error', req => console.log(req))
 stream.on('tweet', tweet => {
@@ -24,6 +40,7 @@ stream.on('tweet', tweet => {
         text: tweet.text
     })
 })
+*/
 
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('home'))
